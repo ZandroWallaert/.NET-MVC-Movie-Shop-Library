b4e389a88fa2ab3b8e763726b07af8350a0231ae
zandro.wallaert <zandro.wallaert@student.howest.be>
2020-05-03 00:24:40 +0200

    Updated library, added services for home page + sessionService

---
+++ Library/Services/MovieService.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using howest_movie_lib.Library.Models;
+
+namespace howest_movie_lib.Library.Services
+{
+    public class MovieService
+    {
+        db_moviesContext context = new db_moviesContext();
+
+        public IEnumerable<Movies> AllMovies()
+        {
+            return context.Movies;
+        }
+
+        public IEnumerable<Movies> AllMoviesByTitle()
+        {
+            return context.Movies.OrderBy(movie => movie.Title);
+        }
+
+        public Movies GetRandomMovie() {
+            Random r = new Random();
+            int rInt = r.Next(1, 250);
+            return (from t in AllMovies() where t.Id.Equals(rInt) select t).First();
+        }
+    }
+    }
